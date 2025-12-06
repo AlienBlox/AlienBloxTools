53237a06e312589df9d2ddde1801b3117596f06d
AlienBlox <alienthebest123@outlook.com>
2025-12-06 19:53:44 +0000

    wee

---
+++ Utilities/ConsoleSetup.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+
+namespace AlienBloxTools.Utilities
+{
+    public class ConsoleSetup
+    {
+        public static void RedirectConsole()
+        {
+            using StreamWriter sw = new($"{Main.SavePath}\\AlienBloxTools\\Logs.txt");
+            Console.WriteLine("Redirecting console...");
+            Console.SetOut(sw);
+            Console.WriteLine("Redirected console");
+        }
+    }
+}
